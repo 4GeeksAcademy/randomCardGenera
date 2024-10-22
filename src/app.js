6cfb3e67e59286bcc6ba6898e0b533d33a5b5635
Redando Dieujuste <101023464+9308RD00@users.noreply.github.com>
2024-10-22 17:55:42 +0000

    new1

--- src/app.js
+++ src/app.js
@@ -6,6 +6,48 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let firstArr = ["♦", "♥ ", "♠", "♣"];
+  let secondArr = ["King", "Qheen", "Jack", 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+  let ind1 = Math.floor(Math.random() * 4);
+  let ind2 = Math.floor(Math.random() * 14);
+
+  let theShow1 = firstArr[ind1];
+  let theShow2 = secondArr[ind2];
+
+  document.querySelector("#top").innerHTML = theShow1;
+  document.querySelector("#bottom").innerHTML = theShow1;
+  document.querySelector("#center").innerHTML = theShow2;
 };
+
+function refresh() {
+  let firstArr = ["♦", "♥ ", "♠", "♣"];
+  let secondArr = ["K", "Q", "J", "A", 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+  let ind1 = Math.floor(Math.random() * 4);
+  let ind2 = Math.floor(Math.random() * 15);
+
+  let theShow1 = firstArr[ind1];
+  let theShow2 = secondArr[ind2];
+
+  document.querySelector("#top").innerHTML = theShow1;
+  document.querySelector("#bottom").innerHTML = theShow1;
+  document.querySelector("#center").innerHTML = theShow2;
+}
+
+document.querySelector("button").addEventListener("click", refresh);
+
+setInterval(function() {
+  let firstArr = ["♦", "♥ ", "♠", "♣"];
+  let secondArr = ["K", "Q", "J", "A", 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+  let ind1 = Math.floor(Math.random() * 4);
+  let ind2 = Math.floor(Math.random() * 15);
+
+  let theShow1 = firstArr[ind1];
+  let theShow2 = secondArr[ind2];
+
+  document.querySelector("#top").innerHTML = theShow1;
+  document.querySelector("#bottom").innerHTML = theShow1;
+  document.querySelector("#center").innerHTML = theShow2;
+}, 1000);
